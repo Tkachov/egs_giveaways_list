063dcc4dc4b1a8aa0cc86d8f56d000081810a62f
Alexander Tkachev <alexander@tkachov.ru>
2023-02-09 18:59:03 +0100

    add 09.02 game

--- static/js/giveaways_list.js
+++ static/js/giveaways_list.js
@@ -1,6 +1,8 @@
 var giveaways_list = [
 	// 2023
 
+	{"start": "09.02.2023", "end": "16.02.2023", "item": "615a4dc5c85c4c28a390dda4298acd80_144f7dd94c37467dbfaa47a55855d9e1"}, // Recipe for Disaster
+
 	{"start": "02.02.2023", "end": "09.02.2023", "item": "e33138cc2b964d38a42a812b83b6507e_6b06cffad08e4f47a8200e06e7fe6101"}, // City of Gangsters
 	{"start": "02.02.2023", "end": "09.02.2023", "item": "87c84bc5f43d4fe69ad8f3ccde0594b0_341b8e920c0d401d98be61a26b78b08c"}, // Dishonored: Death of the Outsider
 
