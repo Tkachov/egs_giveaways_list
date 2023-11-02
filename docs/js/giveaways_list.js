386bf62184ec39dca047788399a5735b294a386a
Alexander Tkachev <alexander@tkachov.ru>
2023-11-02 17:21:40 +0100

    add 02.11 game

--- docs/js/giveaways_list.js
+++ docs/js/giveaways_list.js
@@ -1,6 +1,8 @@
 var giveaways_list = [
 	// 2023
 	
+	{"start": "02.11.2023", "end": "09.11.2023", "item": "80be39e522204fd0942916d73234d044_e55f0dd5f2444c719ab4f4e37fb81d0b"}, // Turnip Boy Commits Tax Evasion
+
 	{"start": "26.10.2023", "end": "02.11.2023", "item": "d1a3ff292b2f4ee289a83d7a91ae3458_e5c0d43b05a0405b802234841da67fd1"}, // Tandem A Tale of Shadows
 	// {"start": "26.10.2023", "end": "02.11.2023", "item": "?"}, // The Evil Within 2
 
