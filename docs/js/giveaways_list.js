55d3ca4681093d4c21bf7e48b3c5dcfd975b8c77
Alexander Tkachev <alexander@tkachov.ru>
2025-12-18 17:58:57 +0100

    18.12

--- docs/js/giveaways_list.js
+++ docs/js/giveaways_list.js
@@ -1,6 +1,8 @@
 var giveaways_list = [
 	// 2025
 
+	// {"start": "18.12.2025", "end": "19.12.2025", "item": "?"}, // Jotunnslayer: Hordes of Hel
+
 	// {"start": "11.12.2025", "end": "18.12.2025", "item": "?"}, // Hogwarts Legacy
 
 	{"start": "04.12.2025", "end": "11.12.2025", "item": "29857990e5434b7db5dd026ef18dbc96_166da88e9a664bafa4a611b883355363"}, // The Darkside Detective
